d8186f10f27a7286f84004f40376b9d70940b784
Divine Dube <divine.dube@takealot.com>
2021-06-16 03:24:24 +0200

    add tooltip to image

--- components/Profile.js
+++ components/Profile.js
@@ -6,15 +6,18 @@ import { server } from '../config'
 export const Profile = ({ author }) => {
     console.log("here")
     console.log(author)
+
     return (
-       <div className={profileStyles.profile}>
+       <div className={profileStyles.profile} >
             <Image
                 src={server + author.profile_photo.thumbnailUrl}
                 layout="responsive"
                 alt={author.name}
                 width={50}
                 height={50}
-            />
+            > 
+            </Image>
+            <span className={profileStyles.tooltiptext}>{author.name}</span>
         </div> 
     )
 }
